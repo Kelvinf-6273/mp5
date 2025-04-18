b0e165a688f5d18355dca12e4894e88547becc26
Kelvinf-6273 <kelvinfang6273@gmail.com>
2025-04-17 23:29:57 -0400

    change7

--- app/[alias]/route.ts
+++ app/[alias]/route.ts
@@ -1,18 +1,32 @@
-import { redirect } from 'next/navigation'
-import getCollection, { LINKS_COLLECTION } from '../lib/mongo'
+
+import getCollection, { LINKS_COLLECTION } from '../lib/mongo';
 
 export async function GET(request: Request): Promise<Response | void> {
-    const url = new URL(request.url)
-    const alias = url.pathname.split('/').pop() || ''
+    try {
+        const url = new URL(request.url);
+        const alias = url.pathname.split('/').pop() || '';
 
-    const collection = await getCollection(LINKS_COLLECTION)
-    const link = await collection.findOne({ alias })
+        const collection = await getCollection(LINKS_COLLECTION);
+        const link = await collection.findOne({ alias });
 
-    if (!link) {
-        console.log(`Alias not found in DB: ${alias}`)
-        return new Response('That shortened link does not exist', { status: 404 })
+        if (!link) {
+            console.log(`Alias not found in DB: ${alias}`);
+
+            return new Response(JSON.stringify({ error: 'That shortened link does not exist' }), {
+                status: 404,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+
+        console.log(`Redirecting ${alias} to ${link.url}`);
+
+        return Response.redirect(link.url, 302);
+    } catch (error) {
+        console.error(error);
+
+        return new Response(JSON.stringify({ error: 'Something went wrong' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
     }
-
-    console.log(`Redirecting ${alias} to ${link.url}`)
-    redirect(link.url)
 }
