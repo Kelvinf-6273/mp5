b78c7baf640e6a05819cfbdceb4d2e5a797cafc2
Kelvinf-6273 <kelvinfang6273@gmail.com>
2025-04-17 22:44:30 -0400

    change3

--- app/[alias]/route.ts
+++ app/[alias]/route.ts
@@ -1,20 +1,16 @@
-import { NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
+import type { NextRequest } from 'next/server'
 import getCollection, { LINKS_COLLECTION } from '../lib/mongo'
 
-export async function GET(
-    request: Request,
-    { params }: { params: { alias: string } }
-) {
+export async function GET(request: NextRequest, { params }: { params: { alias: string } }) {
     const collection = await getCollection(LINKS_COLLECTION)
     const link = await collection.findOne({ alias: params.alias })
 
     if (!link) {
-        console.log(`Alias not found: ${params.alias}`)
-
-        return NextResponse.redirect(new URL('/', request.url))
+        console.log(`Alias not found in DB: ${params.alias}`)
+        return new Response('That shortened link does not exist')
     }
 
     console.log(`Redirecting ${params.alias} to ${link.url}`)
-
-    return NextResponse.redirect(link.url)
+    redirect(link.url)
 }
